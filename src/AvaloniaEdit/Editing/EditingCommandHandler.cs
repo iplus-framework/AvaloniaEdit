fd9ee4d48424b40a4858e2aa3bc8c2b8a1e10622
Damir Lisak <dlisak@kajbum.com>
2025-12-23 10:31:41 +0100

    Build issue fixed because of WPF compatibility in iPlus.

--- src/AvaloniaEdit/Editing/EditingCommandHandler.cs
+++ src/AvaloniaEdit/Editing/EditingCommandHandler.cs
@@ -197,7 +197,7 @@ namespace AvaloniaEdit.Editing
                     {
                         foreach (var segment in segments.Reverse())
                         {
-                            foreach (var writableSegment in textArea.GetDeletableSegments(segment).Reverse())
+                            foreach (ISegment writableSegment in textArea.GetDeletableSegments(segment))
                             {
                                 transformSegment(textArea, writableSegment);
                             }
